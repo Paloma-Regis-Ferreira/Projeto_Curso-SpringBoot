5d514fabc3f9994780c3ae86aa2cffa77feb1660
“Paloma-Regis-Ferreira” <palomaregis29@gmail.com>
2021-12-29 16:27:05 -0300

    camada de servico e registro de componente

--- src/main/java/com/example/demo/course/resources/UserResource.java
+++ src/main/java/com/example/demo/course/resources/UserResource.java
@@ -1,11 +1,16 @@
 package com.example.demo.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.course.entities.User;
+import com.example.demo.course.services.UserService;
 
 //recurso web implementado por um controlador rest
 
@@ -13,11 +18,21 @@ import com.example.demo.course.entities.User;
 @RequestMapping(value = "/users")//nome do recurso
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	//metodo endpoint para acessar usuarios .. retorna respostas de recursos web
 	//identifica metodo get do http
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
-		return ResponseEntity.ok().body(u);//retorna ok e o corpo da resposta
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);//retorna ok e o corpo da resposta
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
