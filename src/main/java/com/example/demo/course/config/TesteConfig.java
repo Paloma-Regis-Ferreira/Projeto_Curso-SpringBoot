eefc1082acb0fa15e253f7fbdf7f59d4f26d2b00
“Paloma-Regis-Ferreira” <palomaregis29@gmail.com>
2021-12-29 15:36:28 -0300

    JPA repository, injecao de dependencia e populacao do banco de dados

---
+++ src/main/java/com/example/demo/course/config/TesteConfig.java
@@ -0,0 +1,27 @@
+package com.example.demo.course.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.example.demo.course.entities.User;
+import com.example.demo.course.repositories.UserRepository;
+
+@Configuration
+@Profile("test")
+public class TesteConfig implements CommandLineRunner {
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "99999999", "123456");
+		User u2 = new User(null, "Paloma Brown", "paloma@gmail.com", "99999999", "789101");
+		
+		userRepository.saveAll(Arrays.asList(u1, u2));
+	} 
+}
