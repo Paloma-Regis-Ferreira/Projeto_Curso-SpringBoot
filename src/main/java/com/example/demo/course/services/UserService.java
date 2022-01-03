ee73796e0546363bdea3818d0f1c466d2526b7c9
Paloma-Regis-Ferreira <palomaregis29@gmail.com>
2022-01-03 12:42:50 -0300

    Atualização de usuario

--- src/main/java/com/example/demo/course/services/UserService.java
+++ src/main/java/com/example/demo/course/services/UserService.java
@@ -29,7 +29,21 @@ public class UserService {
 		return repository.save(obj);
 	}
 	
+	//deleção de usuario
 	public void delete(Long id) {
 		repository.deleteById(id);
+	}
+	
+	//atualizar usuario
+	public User update(Long id, User obj) {
+		User entity = repository.getOne(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setFone(obj.getFone());
 	}
 }
